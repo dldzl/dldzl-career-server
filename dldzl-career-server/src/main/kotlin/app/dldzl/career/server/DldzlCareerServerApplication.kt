2aa41a2ee02f1fc80093c42f6a19791e98115cc6
junsuPark <krpjs0508@gmail.com>
2023-12-04 11:31:22 +0900

    initial commit

---
+++ dldzl-career-server/src/main/kotlin/app/dldzl/career/server/DldzlCareerServerApplication.kt
@@ -0,0 +1,11 @@
+package app.dldzl.career.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DldzlCareerServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DldzlCareerServerApplication>(*args)
+}
---
+++ dldzl-career-server/src/test/kotlin/app/dldzl/career/server/DldzlCareerServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.dldzl.career.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DldzlCareerServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
